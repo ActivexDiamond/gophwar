660fffc8867a9c4134e21b220b1f4bba755fc7bc
ActivexDiamond <activexdiamond@gmail.com>
2023-02-04 01:27:40 +0300

    Syncing bases.

--- src/core/WorldObject.lua
+++ src/core/WorldObject.lua
@@ -1,20 +1,20 @@
 local middleclass = require "libs.middleclass"
-
-local DataRegistry = require "core.DataRegistry"
-local AssetRegistry = require "core.AssetRegistry"
-
-local Event = require "cat-paw.core.patterns.event.Event"
-
-local brinevector = require "libs.brinevector"
+local Object = require "core.Object"
 
 ------------------------------ Helpers ------------------------------
 
 ------------------------------ Constructor ------------------------------
-local Object = middleclass("Object")
-function Object:initialize(id)
-	self.ID = id
-	GAME:getEventSystem():attach(self, Event)
-	DataRegistry:applyStats(self)
+local WorldObject = middleclass("WorldObject", Object)
+---Can take (id, x, y) or (id, vector)
+function WorldObject:initialize(id, a, b)
+	Object.initialize(self, id)
+	if b then
+		self.pos = brinevector(a, b)
+	else
+		assert(brinevector.isVector(a), "Invalid WorldObject initialization arguments. Must be either x/y or Vector2.")
+		self.pos = a:getCopy()
+	end
+	
 end
 
 ------------------------------ Core API ------------------------------
@@ -23,4 +23,4 @@ end
 
 ------------------------------ Getters / Setters ------------------------------
 
-return Object
+return WorldObject
